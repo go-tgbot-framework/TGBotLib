78f978239fcebe9fc1e2d2dbc1ba42db9878795f
pan93412 <pan93412@gmail.com>
2019-01-02 23:20:20 +0800

    [v0.5.2-beta][ISSUES] 修正一個 Type 小問題。

--- TGBotLib_Struct.go
+++ TGBotLib_Struct.go
@@ -40,7 +40,7 @@ type Message struct {
     From            User   `json:"from"`
     Date            int    `json:"date"`
     Chat            Chat   `json:"chat"`
-    ForwardFrom     Chat   `json:"forward_from"`
+    ForwardFrom     User   `json:"forward_from"`
     ForwardFromChat Chat   `json:"forward_from_chat"`
     ForwardDate     int    `json:"forward_date"`
     Text            string `json:"text"`
